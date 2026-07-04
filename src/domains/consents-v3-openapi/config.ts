663fbef69d2e8adac36c6b33d0fe3de4be3fa01c
José Rogério <joserogerio.net@gmail.com>
2026-07-04 12:57:45 -0300

    feat: domínio consents-v3-openapi

---
+++ src/domains/consents-v3-openapi/config.ts
@@ -0,0 +1,6 @@
+export const consentsV3Config = {
+  specName: "consents",
+  specVersion: "3.3.1",
+  url: "https://openbanking-brasil.github.io/openapi/swagger-apis/consents/3.3.1.yml",
+  retryDelaysMs: [2000, 4000, 8000, 16000],
+};
